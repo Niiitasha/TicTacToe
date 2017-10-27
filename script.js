40eac2bf50c54e72fdf98b47acb2831511b1b2c6
Chelsea Bergman <chelseacbergman@gmail.com>
2017-10-27 16:16:17 -0400

    All x working

---
+++ script.js
@@ -0,0 +1,171 @@
+btn1.onclick = function () {
+    clicked1();
+}
+
+function clicked1() {
+    var xOrO = prompt("Please enter either X or O");
+    if (xOrO === "X") {
+        var btnA = document.getElementById("btn1");
+        document.getElementById("btn1").textContent = "X";
+
+    } else if (xOrO === "O") {
+
+
+    } else {
+        var invalid = prompt("Invalid entry. Please enter either X or O");
+    }
+
+}
+
+btn2.onclick = function () {
+    clicked2();
+}
+
+function clicked2() {
+    var xOrO = prompt("Please enter either X or O");
+    if (xOrO === "X") {
+        var btnB = document.getElementById("btn2");
+        document.getElementById("btn2").textContent = "X";
+
+    } else if (xOrO.toLowerCase() === "O") {
+
+
+    } else {
+        var invalid = prompt("Invalid entry. Please enter either X or O");
+    }
+
+}
+
+btn3.onclick = function () {
+    clicked3();
+}
+
+function clicked3() {
+    var xOrO = prompt("Please enter either X or O");
+    if (xOrO === "X") {
+        var btnC = document.getElementById("btn3");
+        document.getElementById("btn3").textContent = "X";
+
+    } else if (xOrO.toLowerCase() === "O") {
+
+
+    } else {
+        var invalid = prompt("Invalid entry. Please enter either X or O");
+    }
+
+}
+
+btn4.onclick = function () {
+    clicked4();
+}
+
+function clicked4() {
+    var xOrO = prompt("Please enter either X or O");
+    if (xOrO === "X") {
+        var btnD = document.getElementById("btn4");
+        document.getElementById("btn4").textContent = "X";
+
+    } else if (xOrO.toLowerCase() === "O") {
+
+
+    } else {
+        var invalid = prompt("Invalid entry. Please enter either X or O");
+    }
+
+}
+
+
+btn5.onclick = function () {
+    clicked5();
+}
+
+function clicked5() {
+    var xOrO = prompt("Please enter either X or O");
+    if (xOrO === "X") {
+        var btnD = document.getElementById("btn5");
+        document.getElementById("btn5").textContent = "X";
+
+    } else if (xOrO.toLowerCase() === "O") {
+
+
+    } else {
+        var invalid = prompt("Invalid entry. Please enter either X or O");
+    }
+
+}
+
+btn6.onclick = function () {
+    clicked6();
+}
+
+function clicked6() {
+    var xOrO = prompt("Please enter either X or O");
+    if (xOrO === "X") {
+        var btnD = document.getElementById("btn6");
+        document.getElementById("btn6").textContent = "X";
+
+    } else if (xOrO.toLowerCase() === "O") {
+
+
+    } else {
+        var invalid = prompt("Invalid entry. Please enter either X or O");
+    }
+
+}
+
+btn7.onclick = function () {
+    clicked7();
+}
+
+function clicked7() {
+    var xOrO = prompt("Please enter either X or O");
+    if (xOrO === "X") {
+        var btnD = document.getElementById("btn7");
+        document.getElementById("btn7").textContent = "X";
+
+    } else if (xOrO.toLowerCase() === "O") {
+
+
+    } else {
+        var invalid = prompt("Invalid entry. Please enter either X or O");
+    }
+
+}
+
+btn8.onclick = function () {
+    clicked8();
+}
+
+function clicked8() {
+    var xOrO = prompt("Please enter either X or O");
+    if (xOrO === "X") {
+        var btnD = document.getElementById("btn8");
+        document.getElementById("btn8").textContent = "X";
+
+    } else if (xOrO.toLowerCase() === "O") {
+
+
+    } else {
+        var invalid = prompt("Invalid entry. Please enter either X or O");
+    }
+
+}
+
+btn9.onclick = function () {
+    clicked9();
+}
+
+function clicked9() {
+    var xOrO = prompt("Please enter either X or O");
+    if (xOrO === "X") {
+        var btnD = document.getElementById("btn9");
+        document.getElementById("btn9").textContent = "X";
+
+    } else if (xOrO.toLowerCase() === "O") {
+
+
+    } else {
+        var invalid = prompt("Invalid entry. Please enter either X or O");
+    }
+
+}
